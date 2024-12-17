7eb9c2f218130f86fdadde70a45d1a01651a054a
bomber-net <bomber-net@mail.ru>
2024-12-17 20:25:13 +0300

    optimize LaravelExtensionsServiceProvider

--- src/Providers/LaravelExtensionsServiceProvider.php
+++ src/Providers/LaravelExtensionsServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace BomberNet\LaravelExtensions\Providers;
 
-use BomberNet\LaravelExtensions\View\Components\App;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use function dirname;
@@ -17,13 +16,11 @@ class LaravelExtensionsServiceProvider extends ServiceProvider
 			{
 				Blade::componentNamespace ('BomberNet\\LaravelExtensions\\View\\Components','laravel-extensions');
 				$this->app->registerDeferredProvider (ViteServiceProvider::class);
-//				$this->loadViewsFrom (dirname (__DIR__).'/resources/views','laravel-extensions');
 				$this->publish ();
 			}
 		
 		protected function publish ():void
 			{
-//				$this->publishes ([dirname (__DIR__).'/resources/views'=>resource_path ('views/vendor/laravel-extensions')],'laravel-extensions-views');
 				$this->publishes ([dirname (__DIR__).'/stubs'=>base_path ('stubs')],'laravel-extensions-stubs');
 			}
 	}
