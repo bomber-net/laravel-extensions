ed83e125214dca5565e5e14f63ff52826b3caaa7
bomber-net <bomber-net@mail.ru>
2024-12-17 19:59:21 +0300

    move balde components from anonymous components to inline components+

--- src/Providers/LaravelExtensionsServiceProvider.php
+++ src/Providers/LaravelExtensionsServiceProvider.php
@@ -14,13 +14,13 @@ class LaravelExtensionsServiceProvider extends ServiceProvider
 		public function boot ():void
 			{
 				$this->app->registerDeferredProvider (ViteServiceProvider::class);
-				$this->loadViewsFrom (dirname (__DIR__).'/resources/views','laravel-extensions');
+//				$this->loadViewsFrom (dirname (__DIR__).'/resources/views','laravel-extensions');
 				$this->publish ();
 			}
 		
 		protected function publish ():void
 			{
-				$this->publishes ([dirname (__DIR__).'/resources/views'=>resource_path ('views/vendor/laravel-extensions')],'laravel-extensions-views');
+//				$this->publishes ([dirname (__DIR__).'/resources/views'=>resource_path ('views/vendor/laravel-extensions')],'laravel-extensions-views');
 				$this->publishes ([dirname (__DIR__).'/stubs'=>base_path ('stubs')],'laravel-extensions-stubs');
 			}
 	}
